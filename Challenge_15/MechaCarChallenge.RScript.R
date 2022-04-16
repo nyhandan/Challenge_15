4bd4b8555f0867cd4131b64a3438c048a1fe3cb0
Dan Nyhan <93630983+nyhandan@users.noreply.github.com>
2022-04-16 14:29:37 -0500

    Add files via upload

---
+++ Challenge_15/MechaCarChallenge.RScript.R
@@ -0,0 +1,24 @@
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg)) 
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI),
+                                          Median=median(PSI),
+                                          Variance=var(PSI),
+                                          SD=sd(PSI),
+                                          .groups = 'keep') 
+total_summary
+lot_summary <- Suspension_Coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),
+                                                                         Median=median(PSI),
+                                                                         Variance=var(PSI),
+                                                                         SD=sd(PSI),
+                                                                          .groups = 'keep') 
+lot_summary
+
+t.test(Suspension_Coil$PSI,mu=1500)
+
+lot1 <- subset(Suspension_Coil, Manufacturing_Lot=="Lot1")
+lot2 <- subset(Suspension_Coil, Manufacturing_Lot=="Lot2")
+lot3 <- subset(Suspension_Coil, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
